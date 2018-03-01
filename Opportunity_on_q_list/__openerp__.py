a648b67d8f0d425e39f6bb8d7e88c8858dfa7201
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-03-01 14:53:52 +0000

    Add files via upload

---
+++ Opportunity_on_q_list/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "Opportunity on Quotations list",
+    'version' : "1.0",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['sale'],
+    'installable' : True,
+    'description' : "This module adds Opportunity to Quotations list view",
+    'website': "www.cier.tech",
+    'data': ['views/opportunity_on_q_tree_v.xml',],
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'summary': 'This module adds Opportunity to Quotations list view',
+    'price': 10.0,
+    'currency': 'EUR',
+}
