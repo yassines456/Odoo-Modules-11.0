03af7f2c853b5ba0a40fbe7045c27282a091883c
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-03-07 14:11:19 +0000

    Add files via upload

---
+++ so_filters_d_w_m/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : "Sales Orders Filters Today, Last Week, Last Month",
+    'version' : "1.0",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['sale'],
+    'installable' : True,
+    'description' : "This module adds three new filters to Quotations and Orders",
+    'website': "www.cier.tech",
+    'data': ['views/so_f_d_w_m.xml',],
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'summary': 'This module adds new filters to Quotations and Orders',
+    'price': 5.0,
+    'currency': 'EUR',
+}
