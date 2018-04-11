4ef5d2e56cb42a5ebe80421af760d6f2ecf96201
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-04-11 08:15:11 +0100

    Add files via upload

---
+++ product_social_share/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2018 Piotr Cierkosz <piotr.w.cierkosz@gmail.com>
+{
+    "name": "Product Share on Social Media",
+    "version": "1.0",
+    "author": "Piotr Cierkosz",
+    "website": "www.cier.tech",
+    "category": "Website",
+    "data": ["views/product_social_share.xml", ],
+    "active": False,
+    'images': ['images/thumbnail.png'],
+    'depends' : ['website_sale'],
+    'installable' : True,
+    'description' : "This module ads opportunity to share products on social media",
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'summary': 'Social media and email share buttons in product',
+    'price': 9.0,
+    'currency': 'EUR',
+}
