edf2ce03d01bc7e602cbd3d9c0b5c4466fff6790
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-06-26 07:55:29 +0100

    Add files via upload
    zip - initial upload

---
+++ zip_on_partner_list/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : "Zip on Contacts List / Tree View",
+    'version' : "1.0",
+    'images': ['images/thumbnail.png'],
+    'author' : "Piotr Cierkosz",
+    'depends' : ['sale'],
+    'installable' : True,
+    'description' : "This module adds Zip code to the Contacts List",
+    'website': "www.cier.tech",
+    'data': ['views/zip_on_contact_lst.xml',],
+    'category': 'Extra Tools',
+    'license': 'Other proprietary',
+    'summary': 'This module adds Zip code to the Contacts List in Contacts, Sales, Purchases and more',
+    'currency': 'EUR',
+}
