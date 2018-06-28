97e3b4ae484ddbc52e3e5a1ea44e3aaefd5c1cc2
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-06-28 15:22:50 +0100

    Add files via upload
    initial commit - the base from v10, updated doc, description and images

---
+++ d_w_m_filters/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : "Today, Last week, Last month filters",
+    'version' : "1.0",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['sale', 'stock', 'account', 'purchase'],
+    'installable' : True,
+    'description' : "This module adds three new filters to Sales, Inventory and Accounting",
+    'website': "www.cier.tech",
+    'data': ['views/so_f_d_w_m.xml', 'views/po_f_d_w_m.xml', 'views/inv_f_d_w_m.xml', 'views/acc_f_d_w_m.xml', 'views/pay_f_d_w_m.xml'],
+    'category': 'Extra Tools',
+    'license': 'Other proprietary',
+    'summary': 'This module adds three new filters to Sales, Inventory and Accounting',
+    'price': 20.0,
+    'currency': 'EUR',
+}
