b7b0d84cd340e78ed242b8847618558b55f88239
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-07-20 14:21:51 +0100

    Add files via upload
    initial commit

---
+++ description_on_meetings_lst/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : "Description on meetings list",
+    'version' : "1.0",
+    'images': ['images/thumbnail.png'],
+    'author' : "Piotr Cierkosz",
+    'category': 'Productivity',
+    'license': 'Other proprietary',
+    'depends' : ['calendar'],
+    'installable' : True,
+    'description' : "This module adds description to the meetings list in Calendar",
+    'website': "www.cier.tech",
+    'summary': 'This module adds description to the meetings list in Calendar',
+    'price': 10.0,
+    'currency': 'EUR',
+    'data': ['views/cal_tree_view_desc.xml',],
+
+}
