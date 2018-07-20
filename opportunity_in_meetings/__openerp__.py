7be72eac1719a90d99b78bd338d3ca1f60c800dc
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-07-20 16:34:00 +0100

    Add files via upload
    port from 10.0

---
+++ opportunity_in_meetings/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# For more information and help please contact author. www.cierkosz.com, www.cier.website_product_attachment_shop
+{
+    'name' : "Opportunity on Meetings form in Calendar",
+    'version' : "1.0",
+    'images': ['images/thumbnail.png'],
+    'author' : "Piotr Cierkosz",
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'summary': 'This module ads option to connect meeting to Opportunity',
+    'price': 10.0,
+    'currency': 'EUR',
+    'depends' : ['sale', 'crm', 'calendar'],
+    'installable' : True,
+    'description' : "This module ads option to connect meeting to Opportunity",
+    'website' : "www.cier.tech",
+    'data': ['views/opp_cal.xml', 'views/opp_cal_prev.xml'],
+
+}
