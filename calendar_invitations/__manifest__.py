75a36105fe6ad8142507c3b562510fa29b839a9a
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-07-23 11:53:39 +0100

    Add files via upload
    port from 11

---
+++ calendar_invitations/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# For more information and help please contact author. www.cierkosz.com, www.cier.website_product_attachment_shop
+{
+    'name' : "Calendar Invitations Tab",
+    'version' : "1.0",
+    'images': ['images/thumbnail.png'],
+    'author' : "Piotr Cierkosz",
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'summary': 'Access customers, send mail, and change meeting status from Invitations Tab',
+    'price': 9.0,
+    'currency': 'EUR',
+    'depends' : ['sale', 'crm', 'calendar'],
+    'installable' : True,
+    'description' : "Access customers from Meetings thanks to Invitations Tab. Invitations tab is now visible to all users.",
+    'website' : "www.cier.tech",
+    'data': ['views/inv_cal.xml'],
+
+}
