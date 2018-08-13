e61aabf06e58809177608405019514b7f6d6f2b8
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-08-13 16:59:10 +0100

    Add files via upload
    initial commit(port from 9.0)

---
+++ credit_on_p_list/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "Credit on Partner List",
+    'version' : "1.0",
+    'images': ['images/thumbnail.png'],
+    'author' : "Piotr Cierkosz",
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'depends' : ['base'],
+    'installable' : True,
+    'description' : "Partner credit visible on the list view",
+    'website': "www.cier.tech",
+    'summary': 'Partner credit visible on the list view',
+    'price': 10.0,
+    'currency': 'EUR',
+    'data': ['views/credit_l_v.xml',],
+
+}
