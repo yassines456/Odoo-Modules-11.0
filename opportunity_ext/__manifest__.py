b912bcec61443fcef937b49db0d4306f04ff2e38
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-09-11 13:52:50 +0100

    Add files via upload
    initial commit

---
+++ opportunity_ext/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# For more information and help please contact author. www.cierkosz.com, www.cier.website_product_attachment_shop
+{
+    'name' : "Opportunity Extension Pack",
+    'version' : "1.0",
+    'live_test_url': 'https://youtu.be/k9UR4Cm1hM0',
+    'images': ['images/thumbnail.png'],
+    'author' : "Piotr Cierkosz",
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'summary': 'Opportunities on Quotations List, Sales Orders form and list and in meetings',
+    'price': 15.0,
+    'currency': 'EUR',
+    'depends' : ['sale_management', 'crm', 'sale', 'calendar'],
+    'installable' : True,
+    'description' : "Get an easy access to opportunities from even more places.",
+    'website' : "www.cier.tech",
+    'data': ['views/opportunity_on_q_tree_v.xml','views/opp_cal.xml', 'views/opp_cal_prev.xml', 'views/so_tree_views.xml','views/so_views.xml','views/opportunity_on_report.xml',],
+}
